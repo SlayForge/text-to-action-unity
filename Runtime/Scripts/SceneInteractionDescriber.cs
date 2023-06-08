67a476b4305bfc1d5507149ea163d10a519de8f6
rabeekiblawi <rabeekiblawi@gmail.com>
2023-06-08 08:17:06 +0300

    refactoring

--- Runtime/Scripts/SceneInteractionDescriber.cs
+++ Runtime/Scripts/SceneInteractionDescriber.cs
@@ -2,25 +2,28 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "New SceneInteractionDescriber", menuName = "ScriptableObject/SceneInteractionDescriber")]
-public class SceneInteractionDescriber : ScriptableObject
+ namespace SlayForge
 {
-    public List<Destination> destinations;
-    public List<SceneObject> interactables;
-
-    [Serializable]
-    public class Destination
+    [CreateAssetMenu(fileName = "New SceneInteractionDescriber", menuName = "ScriptableObject/SceneInteractionDescriber")]
+    public class SceneInteractionDescriber : ScriptableObject
     {
-        public string name;
-        public Vector3 position;
-    }
+        public List<Destination> destinations;
+        public List<SceneObject> interactables;
 
-    [Serializable]
-    public class SceneObject
-    {
-        public string name;
-        public Vector3 position;
-        public bool alreadyInteracted;
-        Action OnInteract;
+        [Serializable]
+        public class Destination
+        {
+            public string name;
+            public Vector3 position;
+        }
+
+        [Serializable]
+        public class SceneObject
+        {
+            public string name;
+            public Vector3 position;
+            public bool alreadyInteracted;
+            Action OnInteract;
+        }
     }
 }
